01f1162eddaee23b99eef037aee562020900a421
BaldStrong <BaldStrong@qq.com>
2023-03-20 20:21:28 +0800

    finish 4.4, commitContainer

---
+++ commit.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func commitContainer(imageName string) {
+	mntURL := "/root/overlayFS/mnt"
+	imageTarURL := "/root/" + imageName + ".tar"
+	// 此处必须要使用-C将tar目录切换到mntURL，如果直接指定mntURL，会将mntURL也带入，
+	// 导致压缩文件包含/root/overlayFS/mnt才到镜像文件所在目录
+	if a, err := exec.Command("tar", "-czf", imageTarURL, "-C", mntURL, ".").CombinedOutput(); err != nil {
+		fmt.Println(a)
+		log.Errorf("tar folder %s error %v", imageTarURL, err)
+	}
+}
